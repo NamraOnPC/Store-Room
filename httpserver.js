24f16f39512bf0a6664ee4f47d67d889471cccdb
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-05-18 13:21:17 -0400

    Create httpserver.js

---
+++ httpserver.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type' , 'text/plain');
+
+res.end('Good Morning, from our server URL\n');
+
+})//.listen(3000);
+
+server.listen(port , hostname, function(){
+
+    console.log(`Server running at http://${hostname}:${port}/`)
+
+
+})
